281201e5b6ec9eb88daac3829aa6d4cab97896ba
rxhma-sys <rahramadhan8@gmail.com>
2025-04-29 16:16:13 +0300

    Fixed component structure

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,18 +1,8 @@
-import React from "react";
-
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
+import React from 'react'
 function TestComponent() {
   return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
+    <div className="TestComponent">
+      <p>This is our new test component.</p>
     </div>
   );
 }
