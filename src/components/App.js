99459ebf146ff9f7723274d1f4ff19c372af695d
rxhma-sys <rahramadhan8@gmail.com>
2025-04-29 16:37:27 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <h1>Now</h1>
       <p className="App-intro">
-        In React apps, we write JSX...
+        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
+        syntax. JSX lets us include JavaScript functions right along with the HTML,
+        and also allows us to add in components, which are separate, self-contained
+        chunks of JSX.
       </p>
       <ExampleComponent />
       <TestComponent />
